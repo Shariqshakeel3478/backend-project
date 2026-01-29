9eef00fe5923b72a45c7b70bec9766e34f18b5f7
Shariq Shakeel <shariq.shakeel@apps.local>
2026-01-29 23:41:34 +0500

    connection made successfully

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async()=>{
+    try {
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       console.log(`MongoDB Connected ${connectionInstance.connection.host}`)  // isko bhi console.log karke parhna hai
+    } catch (error) {
+        console.log("Mongodb connection error",error);
+        process.exit(1) // isko parhna hai
+    } 
+}
+
+export default connectDB
