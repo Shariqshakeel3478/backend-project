f152dfb6d044be6a7ee77fa9e932b266235ab9c2
Shariq Shakeel <shariqshakeel107@gmail.com>
2026-04-18 21:49:32 +0500

    Update controllers added

---
+++ src/models/subscription.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const subSchema = new mongoose.Schema({
+
+    subscriber:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    channel:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+},{timestamps:true})
+
+export const Subscription = mongoose.model("Subscription",subSchema)
